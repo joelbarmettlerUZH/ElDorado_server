92e69861217fc80b4647753ef87ce5290cdfa4ff
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-03-31 16:12:38 +0200

    #6 added empty test classes for game.main classes

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs18/game/main/BlockadeTest.java
@@ -0,0 +1,16 @@
+package ch.uzh.ifi.seal.soprafs18.game.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlockadeTest {
+
+    @Test
+    public void deactivate() {
+    }
+
+    @Test
+    public void assign() {
+    }
+}
