03fd5c852f160f5a1736f19dd6514517e5df09aa
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-04-20 16:38:16 +0200

    enadle CORS #27

--- src/main/java/ch/uzh/ifi/seal/soprafs18/controller/PlayerController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/controller/PlayerController.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class PlayerController  implements Serializable {
     private final String context = CONSTANTS.APICONTEXT + "/Player";
