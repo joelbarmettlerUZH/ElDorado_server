4231bd42c9e8a1493440f9c2ca190bdf471f987c
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-04-25 09:45:37 +0200

    made history JSON ignore for now #24

--- src/main/java/ch/uzh/ifi/seal/soprafs18/game/player/Player.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/game/player/Player.java
@@ -166,6 +166,7 @@ public class Player implements Serializable {
     Each time the user plays a Card of any type, its history is appended with the corresponding CardAction.
      */
     // @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
     @Transient
     private List<CardAction> history;
 
