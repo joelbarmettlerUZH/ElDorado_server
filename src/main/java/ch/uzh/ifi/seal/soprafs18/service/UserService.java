c1fdeff993520e5f5d2f2679fc5e9c2c4b4ceab8
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-05-07 21:18:15 +0200

    exchange TESTTOKEN by true token #37

--- src/main/java/ch/uzh/ifi/seal/soprafs18/service/UserService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/service/UserService.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -58,7 +59,17 @@ public class UserService  implements Serializable {
     }
 
     public List<String> createUser(UserEntity user) {
-        String token = "TESTTOKEN";
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String token = buffer.toString();
         LOGGER.severe("Setting token of user " + user.getUserID() + " to " + token);
         user.setToken(token);
         userRepository.save(user);
