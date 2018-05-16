d9aa8b7512bd31dea4b1a05849c93fde26a41290
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-05-16 08:49:42 +0200

    prevent fake buying history #24

--- src/main/java/ch/uzh/ifi/seal/soprafs18/game/player/Player.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs18/game/player/Player.java
@@ -424,8 +424,8 @@ public class Player implements Serializable {
             // history.add(new CardAction(slot.getCard(), "Sell: " + slot.getCard().getName()));
 
             if (slot.getCard().getCoinCost() <= coins) {
-                Card card = this.board.getMarketPlace().buy(slot);
                 this.addToHistory(new CardAction(slot.getCard(), "Buy"));
+                Card card = this.board.getMarketPlace().buy(slot);
                 this.discard(card);
                 this.coins = 0.0f;
                 bought = true;
